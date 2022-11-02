6db1351a9623a1009b82ee0805f571e0fa590362
alexis0501 <alexismayoral11@gmail.com>
2022-11-02 15:59:26 -0700

    Updated text animation

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -3,11 +3,11 @@ import './header.css';
 function Header() {
   return (
     <div id='home' className='container header-container'>
-      <h3>
+      <h3 className=''>
         <p>
           I'm
         </p>
-        Alexis Mayoral
+        <span>Alexis Mayoral</span>
       </h3>
       <h2>Full-Stack Web Developer</h2>
     </div>
