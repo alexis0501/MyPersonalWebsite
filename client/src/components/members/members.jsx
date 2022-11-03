b6e69c3eaab0477bdd7a8582bce61e6c15d7de75
alexis0501 <alexismayoral11@gmail.com>
2022-11-03 15:31:39 -0700

    Added description to all 3 projects

--- client/src/components/members/members.jsx
+++ client/src/components/members/members.jsx
@@ -7,13 +7,17 @@ const Members = () => {
       <div className="member member-1">
         <div className="member-img"></div>
         <div className="member-info">
-          <h1 className="name">Car Dealership</h1>
-          <h3 className="position">By Alexis</h3>
+          <h1 className="name">Car Wiki</h1>
+          <h3 className="position">JavaScript</h3>
           <h4 className="about">
-              A Car Dealership website where you are able to sell a vehicle of your choosing. I took inspiration from my passion of cars to create this
-              in MERN. Using Axios to handle the routes, Node.js as my back-end with React as my front-end. When a user sells a vehicle it is stored in 
-              MongoDB.
+              A Car Wiki where a person is able to create posts about their favorite vehicles for others to see and obtain data based
+              on the vehicle that is selected. Utilizing Bootstrap for responsive designs for user satisfaction. Using JavaScript for both 
+              front-end (Client) with React & back-end (Server) with NodeJS. Creating my own API for post information to be sent to my 
+              MongoDB database & verifying json information with Postman.
+              <br />
+              Languages & Tools: JavaScript, Bootstrap, React, NodeJS, MongoDB, Mongoose, Postman, VsCode & Git.
           </h4>
+          {/* Going to add link to the website  */}
           <a href="#contact" className="contact-member">
             <span>contact</span>
           </a>
@@ -23,11 +27,16 @@ const Members = () => {
       <div className="member member-2">
         <div className="member-img"></div>
         <div className="member-info">
-          <h1 className="name">Project 2</h1>
-          <h3 className="position">By Alexis</h3>
+          <h1 className="name">Blog Website</h1>
+          <h3 className="position">Java</h3>
           <h4 className="about">
-            Description here
+            A Blog Website where people are able to share what is going on in their daily lives with login & registration to store a user's session
+            Using Session I am able to have conditional rendering for certain pages. As well as having redirects for unwanted visitors who 
+            have not signed in. Having multiple SQL query statements to join data together with Jinja2.
+            <br />
+            Languages & Tools: Java 8, Git, Spring Tool Suite, Bootstrap, MySQL, Bcrypt & Session.
           </h4>
+          {/* Going to add link to the website */}
           <a href="#contact" className="contact-member">
             <span>contact</span>
           </a>
@@ -37,10 +46,14 @@ const Members = () => {
       <div className="member member-3">
         <div className="member-img"></div>
         <div className="member-info">
-          <h1 className="name">Project 3</h1>
-          <h3 className="position">By Alexis</h3>
+          <h1 className="name">Student Tracker</h1>
+          <h3 className="position">Python</h3>
           <h4 className="about">
-            Description here
+            A Student Tracker with Flask framework where a teacher is able to add Students to their classroom using query join statements with MySQL
+            and utilizing session to add said student to the classroom of the user who is signed in. When a user wants to sign in, 
+            their passwords get hashed with Bcrypt for security & using Flash to display error messages.
+            <br />
+            Languages & Tools: Python, Bcrypt, Jinja, Flask, Flash, Mysql, Git, & Bootstrap.
           </h4>
           <a href="#contact" className="contact-member">
             <span>contact</span>
